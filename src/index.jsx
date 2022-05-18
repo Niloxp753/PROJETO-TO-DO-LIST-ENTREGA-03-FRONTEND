426bca321e89fdaa11b78b9aaba5056e781c559e
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-17 21:35:53 -0300

    index update

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Home from "./views/Home/Home";
 import "assets/styles/main.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
